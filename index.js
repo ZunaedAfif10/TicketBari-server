adf1331baae5af3f2df0205d59b859159fec0a2b
ZunaedAfif10 <zunaedrahman7@gmail.com>
2026-06-25 01:18:58 +0600

    added tickets api

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express()
-const port = 3000
+const port = 5000
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+
+app.use(cors());
+// app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -19,16 +23,29 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("ticketbari");
+    const ticketCollection = database.collection("tickets");
+
+
+    app.get('/api/tickets', async (req, res) => {
+      const cursor = ticketCollection.find();
+      const companies = await cursor.toArray();
+      console.log(companies);
+      res.send(companies)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
